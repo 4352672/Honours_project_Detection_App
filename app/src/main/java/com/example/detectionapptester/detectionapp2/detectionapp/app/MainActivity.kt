02132d2be1ccccbe6ea1ffb02d341d88073a929c
Adam4352672 <4352672@myuwc.ac.za>
2026-05-13 01:17:21 +0200

    This is phase 1 which consisted of the following Project Configuration,Permissions and App Identity,The UI Layout,drawable Resource,Adding of the buttons and the visual styling

--- app/src/main/java/com/example/detectionapptester/detectionapp2/detectionapp/app/MainActivity.kt
+++ app/src/main/java/com/example/detectionapptester/detectionapp2/detectionapp/app/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.detectionapptester.detectionapp2.detectionapp.app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnScan = findViewById<Button>(R.id.btnScan)
+        val btnInfo = findViewById<Button>(R.id.btnInfo)
+        val btnExit = findViewById<Button>(R.id.btnExit)
+        //Scan
+        btnScan.setOnClickListener { Toast.makeText(this, "scan", Toast.LENGTH_SHORT).show() }
+        //Info
+        btnInfo.setOnClickListener {
+        AlertDialog.Builder(this)
+            .setTitle("About Detector")
+            .setMessage(
+                "Detector Intrusion Detection System.\n\n" +
+                "This app will scan the apps for malware and viruses"
+            )
+            .setPositiveButton("OK", null)
+            .show()
+
+        }
+        //Exit
+        btnExit.setOnClickListener {
+            finishAffinity()
         }
     }
 }
